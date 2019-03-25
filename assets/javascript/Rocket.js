b3705193c6e80f3e34036c27871b28705c5725b3
ryan.w.daly <ryan.w.daly90@gmail.com>
2019-03-25 03:31:14 -0400

    more stufff

---
+++ assets/javascript/Rocket.js
@@ -0,0 +1,25 @@
+function Rocket() {
+    this.pos = createVector(width/2, height);
+    this.vel = createVector();
+    this.acc = createVector();
+
+
+    this.applyForce = function(force) {
+        this.acc.add(force)
+    }
+
+    this.update = function() {
+        this.vel.add(this.acc);
+        this.pos.add(this.vel);
+        this.acc.mult(0);
+    }
+
+    this.show = function() {
+        push();
+        translate(this.pos.x, this.pos.y);
+        rotate(this.vel.heading());
+        rectMode(CENTER);
+        rect(0, 0, 50, 10)
+        pop();
+    }
+}
