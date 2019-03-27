8e9125d05b8434515712be9265824edf58d64ce1
ryan.w.daly <ryan.w.daly90@gmail.com>
2019-03-26 20:50:03 -0400

    up and running

--- assets/javascript/Rocket.js
+++ assets/javascript/Rocket.js
@@ -1,25 +1,32 @@
-function Rocket() {
-    this.pos = createVector(width/2, height);
-    this.vel = createVector();
-    this.acc = createVector();
+class Rocket {
+    constructor(lifespan) {
+        this.pos = createVector(width/2, height);
+        this.vel = createVector();
+        this.acc = createVector();
+        this.dna = new DNA(lifespan);
+        this.fitness = 0;
 
-
-    this.applyForce = function(force) {
+    }
+    applyForce(force) {
         this.acc.add(force)
     }
+    update(count) {
 
-    this.update = function() {
+        this.applyForce(this.dna.genes[count]);
         this.vel.add(this.acc);
         this.pos.add(this.vel);
         this.acc.mult(0);
     }
-
-    this.show = function() {
+    show() {
         push();
         translate(this.pos.x, this.pos.y);
         rotate(this.vel.heading());
         rectMode(CENTER);
-        rect(0, 0, 50, 10)
+        triangle(0, 0, 0, 6, 12, 3)
         pop();
+    }
+    calcFitness() {
+        let d = dist(this.pos.x, this.pos.y, target.x, target.y)
+        this.fitness = 1/d;
     }
 }
--- assets/javascript/sketch.js
+++ assets/javascript/sketch.js
@@ -1,14 +1,33 @@
+const popSize = 50;
+const mutationRate = 0;
+const lifespan = 200;
+
+var target;
 var rocket;
+var population;
+var lifeP;
+var count = 0; 
 
 function setup() {
-    createCanvas(400,300);
-    rocket = new Rocket();
+    // frameRate(10)
+    createCanvas(600, 400);
+    population = new Population(mutationRate, popSize, lifespan)
+    lifeP = createP();
+    target = createVector(width/2, 50)
 }
-
 function draw() {
     background(0);
-    rocket.update()
-    rocket.show();
-
+    ellipse(target.x, target.y, 16, 16)
+    if (count === lifespan) {
+        population.evaluate();
+        population.generateNextGen();
+        count = 0;
+    }
+    count++;
+    lifeP.html(count);
+    for (let i = 0; i < popSize; i++) {
+        population.stack[i].update(count);
+        population.stack[i].show();
+    }
 }
 
